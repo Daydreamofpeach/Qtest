4ce670567c619d7a14867dc53634d096bfaf0665
Daydreamofpeach <ethanjamescullen@outlook.com>
2023-05-12 13:48:37 +1000

    Initial commit

---
+++ src/layouts/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <DesktopNavigation />
+    <MobileNavigation />
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  components: {
+    DesktopNavigation: () => import('components/navigation/desktop/Header'),
+    MobileNavigation: () => import('components/navigation/mobile/Menu'),
+  },
+};
+</script>
